44832d4c10c89a3203b00f2318b06bef7822f155
Annie Bourke <aabourke@gmail.com>
2018-05-11 19:01:54 +0000

    automatically backed up by learn

--- app/models/show.rb
+++ app/models/show.rb
@@ -4,7 +4,8 @@ class Show < ActiveRecord::Base
   end
 
   def self.most_popular_show
-
+    highest_rating = self.highest_rating
+    Show.where(rating: highest_rating)
   end
 
   def self.lowest_rating
